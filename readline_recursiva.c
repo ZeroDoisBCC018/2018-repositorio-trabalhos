0023c31267740a35cc5e243e5364e1b5f3b44ea3
João Villaça <38091668+ZeroDoisBCC018@users.noreply.github.com>
2018-06-05 20:32:03 -0300

    Update readline_recursiva.c

--- readline_recursiva.c
+++ readline_recursiva.c
@@ -1,56 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int equal(char *a, char *b){
-	if(a == b){
-		return 1;
-	} else {
-		return 0;
-	}
+int length(char *str) {
+  int count = 0;
+  while (str[count] != '\n') {
+    count++;
+  }
+  return count;
 }
-int length(char *str){
-	int i = 0;
-	int count = 0;
-	while(str[i] != '\n'){
-		i++;
-		count++;
-	}
-	return count;
-}
+void read_recursiva(char *str, int i) {
+  str = (char *)realloc(str, (i + 1) * sizeof(char));
+  str[i] = getchar();
 
-void read_recursiva(char *str, int i){
-	str = (char*)realloc(str,(i+1)*sizeof(char));
-	scanf(" %c", &str[i]);
-	
-	if(str[i] == '\n'){
-		str[i] = '\0';
-		return;
-	} 
-	i++;
-	read_recursiva(str, i);
+  if (str[i] == '\n' || str[i] == '\0') {
+    str[i] = '\0';
+    return;
+  }
+  read_recursiva(str, i + 1);
+  return;
 }
-
-char *read_line(){
-	char *str = NULL;
-	read_recursiva(str, 0);
-	return str;
+char *read_line() {
+  char *str = NULL;
+  read_recursiva(str, 0);
+  return str;
 }
+int equal(char *a, char *b) {
+  int x, y;
+  x = length(a);
+  y = length(b);
+  if (x != y) return 0;
+  for (int i = 0; i < x; i++) {
+    if (a[i] != b[i]) return 0;
+  }
+  return 1;
+}
+int main(int argc, char const *args[]) {
+  char *str1;
+  str1 = read_line();
+  char *str2;
+  str2 = read_line();
 
-int main(int argc, char const *args[]){
-	char *str1 = read_line();
-	length(str1);
-	
-	char *str2 = read_line();
-	length(str2);
-	
-	int r = equal(str1, str2);
-		if(r == 1){
-			printf("Linhas 1 e 2: %s", str1);
-		} else {
-		printf("Linha 1: %s\n", str1);
-		printf("Linha 2: %s\n", str2);
-		}
-	free(str1);
-	free(str2);
-	return 0;
+  int r = equal(str1, str2);
+  if (r == 1) {
+    printf("Linhas 1 e 2: %s", str1);
+  } else {
+    printf("Linha 1: %s\n", str1);
+    printf("Linha 2: %s\n", str2);
+  }
+  free(str1);
+  free(str2);
+  return 0;
 }
