b3f854009b2cf923e7304d685c583e6534852e01
João Villaça <38091668+ZeroDoisBCC018@users.noreply.github.com>
2018-06-12 23:01:55 -0300

    Add files via upload

---
+++ trabalhos_icc_1/trabalho4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+	char type;
+	short int a1, a2, b1, b2, c1, c2, d1, d2;
+}parameter;
+
+int gaussian_scaling(){
+
+}
+
+int main(int argc, char const *argv[]){
+
+	short int noequ  = scanf("%d ", &noequ);
+	unsigned short int nocoef = scanf("%hu ", &nocoef);
+	short int i, j;
+
+	parameter *vector_access = (parameter*)malloc(sizeof(parameter)*noequ);
+
+	for(i=0; i<noequ; i++){
+		vector_access[i].type = getchar();
+		vector_access[i].a1 = scanf("%hd ", &vector_access[i].a1);
+		vector_access[i].a2 = scanf("%hd ", &vector_access[i].a2);
+		vector_access[i].b1 = scanf("%hd ", &vector_access[i].b1);
+		vector_access[i].b2 = scanf("%hd ", &vector_access[i].b2);
+		vector_access[i].c1 = scanf("%hd ", &vector_access[i].c1);
+		vector_access[i].c2 = scanf("%hd ", &vector_access[i].c2);
+		vector_access[i].d1 = scanf("%hd ", &vector_access[i].d1);
+		vector_access[i].d2 = scanf("%hd ", &vector_access[i].d2);
+	}
+}
