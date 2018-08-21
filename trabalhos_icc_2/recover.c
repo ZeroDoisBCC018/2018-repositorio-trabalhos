642f262e62238701c7597ed399864bdd239301e6
João Villaça <38091668+ZeroDoisBCC018@users.noreply.github.com>
2018-08-20 21:20:13 -0300

    Update recover.c

--- trabalhos_icc_2/recover.c
+++ trabalhos_icc_2/recover.c
@@ -2,32 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-void* jpg_recover(FILE* f, int count){
-	char n[7];
-	sprintf(n, "%3d.jpg", count);
-	fopen(n, wb);
-	fwrite
+void* jpg_recover(FILE* f, int count) {
+  char n[7];
+  sprintf(n, "%3d.jpg", count);
+  fopen(n, wb);
+  fwrite
 }
 
-int main(void){
-	
-	int count = 0;
-	FILE* fp = NULL;
-	unsigned char dump, img[512];
-	
-	do{
-		scanf("%c", &dump);
-		if(dump == '0xff'){
-			do{
-				img[0] = dump;
-				for(int i = 1; i<512; i++){
-					scanf("%c", &img[i]);
-				}
-					jpg_recover(fp, count);
-					count++;
-				}while(scanf(stdin) != EOF);
-			}
-		}			
-	}while(scanf(stdin) != EOF);		
-	return EXIT_SUCCESS;
+int main(void) {
+  int count = 0;
+  FILE* fp = NULL;
+  unsigned char dump, img[512];
+
+  do {
+    scanf("%c", &dump);
+    if (dump == '0xff') {
+      scanf("%c", &dump);
+      if (dump == '0xd8') {
+		img[0] = '0xff';
+        img[1] = dump;
+
+        do {
+          while(){
+            scanf("%c", &img[i]);
+          }
+          jpg_recover(fp, count);
+          count++;
+        } while (scanf(stdin) != EOF);
+      }
+    }
+  } while (scanf(stdin) != EOF);
+  return EXIT_SUCCESS;
 }
