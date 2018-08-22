a2f9853f6dd5fe764a3058a5a9252a9775bf8f9c
João Villaça <38091668+ZeroDoisBCC018@users.noreply.github.com>
2018-08-22 08:55:12 -0300

    Update recover.c

--- trabalhos_icc_2/recover.c
+++ trabalhos_icc_2/recover.c
@@ -3,13 +3,13 @@
 #include <string.h>
 
 int main(void) {
-  int r, count = 0;
+  int r, s = -1, count = 0;
   char img[512], p_name[8];
   FILE* fp = NULL;
 
   do {
     r = scanf("%512c", img);
-
+//nao ta entrando nesse if
     if (img[0] == 0xff && img[1] == 0xd8 && img[2] == 0xff) {
       sprintf(p_name, "%03d.jpg", count);
       count += 1;
@@ -18,12 +18,12 @@ int main(void) {
       if (fp != NULL) {
         fwrite(img, 512, sizeof(char), fp);
       }
+      s = fclose(fp);
+      fp = NULL;
     }
   } while (r != EOF);
 
-  if (fp != NULL) {
-    fclose(fp);
-  } else {
+  if (s == -1) {
     printf("Could not find pictures\n");
   }
 
