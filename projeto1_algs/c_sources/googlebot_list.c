0bdc7d92e26048d24313e2400e3d99a6624025a6
BrunoBaldissera <38329588+BrunoBaldissera@users.noreply.github.com>
2018-09-19 20:49:44 -0300

    Parâmetros das funções consertados, protótipos de algumas funções adicionados (Search e Insert), função UpdateRelevance adicionada

--- projeto1_algs/c_sources/googlebot_list.c
+++ projeto1_algs/c_sources/googlebot_list.c
@@ -1,23 +1,38 @@
 struct node_ {
 	SITE site;
 	NODE* next; 
-}
+}node_;
 
 struct list_ {
 	NODE* first;
 	NODE* last;
 	int size;
+}list_;
+
+void SortList(LIST* l){ //FAZER
 }
 
-void SortList(LIST l){
+int SearchCode(LIST* l){//FAZER
+	
 }
 
+int InsertSite(LIST* l){ //FAZER
+	
+}
 LIST CreateList(void){
 	LIST l;
 	l->first = NULL;
 	l->last = NULL;
 	l->size = 0;
 	return l;
+}
+
+int CheckEmptyList(NODE* first){
+	return (first == NULL) ? 1 : 0;
+}
+
+int CheckInvalidSize(LIST* l){ // this functions as both maximum size check and invalid (negative) size check
+	return (l->size > 9999 || l->size < 0) ? 1 : 0;
 }
 
 void EraseList (LIST* l)
@@ -53,11 +68,19 @@ void EraseList (LIST* l)
 	}
 }*/
 
-int CheckEmptyList(NODE* first){
-	return (first == NULL) ? 1 : 0;
+int UpdateRelevance(LIST* l, int code){
+	if (CheckEmptyList == 1) return ERROR;
+	if (code < 0 || code > 1000) return ERROR;
+	NODE* aux;
+	aux = l->first;
+	int newrel, check = 0;
+	for (int i = 0; i < l->size; i++){
+		if(aux->site->code == code){
+			scanf("%d", &newrel);
+			aux->site->code = newrel;
+			check = 1;
+		}
+	}
+	if (check != 1) return ERROR;
+	else return 0;
 }
-
-int CheckInvalidSize(LIST l){ // this functions as both maximum size check and invalid (negative) size check
-	return (l->size > 9999 || l->size < 0) ? 1 : 0;
-}
-
