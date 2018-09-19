66eb96be187e04b51b3728774b1724722e46aa61
brugazoni <38261348+brugazoni@users.noreply.github.com>
2018-09-18 23:00:00 -0300

    Update googlebot_csv.c

--- projeto1_algs/c_sources/googlebot_csv.c
+++ projeto1_algs/c_sources/googlebot_csv.c
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include "googlebot_ds.h"
+#define TRUE 1
+#define FALSE 0
 
 typedef FILE* CSV;
 typedef SITE* OBJ;
 
-CSV gb_openCSV (CSV fp, char* filename){
+CSV gb_OpenCSV (CSV fp, char* filename){
 	
 	if(filename == NULL){
 		fprintf(stdout, "No filename given!\n");
@@ -27,9 +29,9 @@ CSV gb_openCSV (CSV fp, char* filename){
 	return fp;
 }
 
-void* gb_readCSV (CSV fp, OBJ o){
-	
-	int i = 0, j = 0;
+void* gb_ReadCSV (CSV fp, OBJ o){
+	//falta criar o vetor de sites
+	int i = 0, j = 0, k = 0;
 	char dump = ' '; // dump is initialized as blank space so it certainly enters the reading loop on line x
 	
 	if(fp == NULL){
@@ -40,18 +42,24 @@ void* gb_readCSV (CSV fp, OBJ o){
 	fseek(fp, 0, SEEK_SET);
 	
 	do{
-		fscanf(fp, "%d,%m[^,],%d,%m[^,],", &OBJ->code, OBJ->name, &OBJ->relev, OBJ->link);
+		fscanf(fp, "%d,%m[^,],%d,%m[^,],", &o->code, o->name, &o->relev, o->link);
+		//quebrar em 2 linhas isolando o 'code', chamando a função gb_CodeCheck
 		
 		while (dump != '\n'){
 			dump = fgetc(fp);
-			OBJ->keyw[i][j] = dump;
+			o->keyw[j][k] = dump;
 			
-			j++;
+			k++;
 			if(dump == ','){
-				 i++;
-				 j = 0;
+				 j++;
+				 k = 0;
 			}
 		}
 	}while(!feof(fp));
 	
+}
+
+int gb_CodeCheck (int code, OBJ array){
+	//vamos comparar o 'code' com todos os anteriores do vetor antes de inseri-lo neste
+	//precisaremos de um contador de quantas structs ja foram armazenadas
 }
