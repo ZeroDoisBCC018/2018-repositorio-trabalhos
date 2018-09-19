248e1b9ec1864d4fe475a0f40ad9bc73e5554be6
brugazoni <38261348+brugazoni@users.noreply.github.com>
2018-09-19 16:54:33 -0300

    Add files via upload

---
+++ projeto1_algs/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (int argc, char* argv[])
+{
+
+	return 0;
+}
+
+/*
+ * fprintf(stdout, "No filename given!\n");
+   fprintf(stdout, "Instructions on how to use GBSiteRelevance:\n");
+   fprintf(stdout, "On console command line, type: ");
+   fprintf(stdout, "./GBSiteRelevance <csv_file>\n");
+		
+   perror("Memory allocation error/file can't be opened.\n");
+
