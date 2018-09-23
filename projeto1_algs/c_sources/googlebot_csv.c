31f421ba1625c33106c34fba46ef7119d4b16501
João Villaça <38091668+ZeroDoisBCC018@users.noreply.github.com>
2018-09-23 14:51:15 -0300

    Rever GB_ReadCSV.
    Tambem falta acabar as funcoes GB_UpdateDataBase e GB_CodeCheck.

--- projeto1_algs/c_sources/googlebot_csv.c
+++ projeto1_algs/c_sources/googlebot_csv.c
@@ -8,55 +8,43 @@
 #include <string.h>
 #include "googlebot_ds.h"
 #include "googlebot_lists.h"
-
+typedef int boolean;
 typedef FILE* CSV;
 
-int GB_CodeCheck (int code, SITE* array){
+boolean GB_CodeCheck (int code, SITE* array){
 	//vamos comparar o 'code' com todos os anteriores do vetor antes de inseri-lo neste
 	//precisaremos de um contador de quantas structs ja foram armazenadas
 }
 
 
 CSV GB_OpenCSV (CSV fp, char* filename){
-	
 	if(filename == NULL){
 		perror(".csv filename not given.\n");
 		exit(EXIT_FAILURE);
 	}
-	
 	fp = fopen(filename, "r+");
-	
 	if(fp == NULL){
 		perror(".csv file could not be opened. Not enough memory/fatal error ocurred while trying to access the disk.\n");
 		exit(EXIT_FAILURE);
 	}
-	
 	return fp;
 }
 
-void* GB_ReadCSV (CSV fp, SITE* s){
-	
+void GB_ReadCSV (CSV fp, SITE* s){
 	int i = 0, j = 0, k = 0;
 	char dump = ' '; // dump is initialized as blank space so it certainly enters the reading loop on line x
-	
 	if(fp == NULL){
 		perror("File not opened.\n");
 		exit(EXIT_FAILURE);
 	}
-	
 	fseek(fp, 0, SEEK_SET);
-	
 	do{
 		fscanf(fp, "%d", &s->code);
-		
 		if (!GB_CodeCheck(s->code, )) return NULL;
-		
 		fscanf(fp, ",%m[^,],%d,%m[^,],", o->name, &o->relev, o->link);
-		
 		while (dump != '\n'){
 			dump = fgetc(fp);
 			s->keyw[j][k] = dump;
-			
 			k++;
 			if(dump == ','){
 				 j++;
@@ -64,8 +52,7 @@ void* GB_ReadCSV (CSV fp, SITE* s){
 			}
 		}
 	}while(!feof(fp));
-	
 }
 
-int GB_UpdateDataBase () { /* TODO */
+boolean GB_UpdateDataBase () { /* TODO */
 }
