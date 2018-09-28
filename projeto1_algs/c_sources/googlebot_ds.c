0f81f87c62e9b9f0a5d3205a19ac121705b026ee
BrunoBaldissera <38329588+BrunoBaldissera@users.noreply.github.com>
2018-09-27 21:37:08 -0300

    Update googlebot_ds.c

--- projeto1_algs/c_sources/googlebot_ds.c
+++ projeto1_algs/c_sources/googlebot_ds.c
@@ -45,3 +45,32 @@ int GB_InsertSite(LIST* l, int pos){
 	InsertNodeAtPos(l, new, pos);
 	return SUCCESS;
 }
+
+int GB_RemoveSite(LIST* l, int code){
+	if (GB_CheckInvalidList(l)) return ERROR; 
+	if (GB_CheckEmptyList(l)) return ERROR;
+	NODE* aux1 = l->first; 
+	NODE* aux2 = aux1->next;	
+	if (aux1->code == code){ 		   //checks if the first node is to be erased, which will have 2 different cases
+		int size = GB_CountListSize(l);
+		if (size == 1) free(aux1);         //1: the list only has this node, so we only need to free it
+		else{				   //2: the list has more elements
+			l->first = aux2;
+			aux1->next = NULL;
+			free(aux1);
+			return SUCCESS;
+		}
+	}
+	while (aux2->code != pos && aux2 != NULL){ //runs through the list until it ends or until the code is found
+		aux1 = aux1->next;
+		aux2 = aux2->next;
+	}
+	if (aux2 == NULL) return ERROR;            //if the code was not found, error is returned
+	if (aux2->code == code){		   //if the code was found, we start removing the node, but only after
+		aux1->next = aux2->next;           //linking correctly the nodes after and before the one to be removed
+		aux2->next = NULL;
+		free(aux2);
+		return SUCCESS;
+	}
+	return ERROR;				   //if the function hasn't returned success yet, it has failed somewhere, so 
+}					           //we return error
