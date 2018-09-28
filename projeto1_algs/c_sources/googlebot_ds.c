80371f86b8122fd1f7ce7a92c893a30d3cbbc78d
BrunoBaldissera <38329588+BrunoBaldissera@users.noreply.github.com>
2018-09-27 21:47:35 -0300

    Update googlebot_ds.c

--- projeto1_algs/c_sources/googlebot_ds.c
+++ projeto1_algs/c_sources/googlebot_ds.c
@@ -13,36 +13,38 @@ struct site_ {
 	const char* name;
 	int relev;
 	const char* link;
-	const char keyw[500];
+	const char keyw[10][50];
 }
 
 NODE* GB_GetSiteStdin(void) {
 	if (l == NULL) return ERROR;
 	if (GB_CheckInvalidList(l)) return ERROR;
-	NODE* new = GB_NewNode(void);
+	NODE* new = GB_NewNode(void);		  /*a new node is created*/
 	
-	int code = fscanf(stdin, "%d", &code);
+	int code = fscanf(stdin, "%d", &code);	  /*we read from stdin the code and allocate it in the new node's site position*/	
 	new->code = code;
 	fflush(stdin);
 	
-	char* name = fgets(name, 50, stdin);
+	char* name = fgets(name, 50, stdin);	  /*we read from stdin the name and allocate it in the new node's site position*/
 	new->name = name;
 	fflush(stdin);
 	
-	int relev = fscanf(stdin, "%d", &relev);
+	int relev = fscanf(stdin, "%d", &relev);  /*we read from stdin the relevance and allocate it in the new node's site position*/
 	new->relev = relev;
 	fflush(stdin);
 	
-	char* link = fgets(link, 100, stdin);
+	char* link = fgets(link, 100, stdin);     /*we read from stdin the link and allocate it in the new node's site position*/
 	new->link = link;
 	fflush(stdin);
+	
+	//char* keywords =  TODOTODOTODO
 	return new;
 }
 
-int GB_InsertSite(LIST* l, int pos){
+int GB_InsertSite(LIST* l, int pos){	          
 	if (GB_CheckInvalidList(l)) return ERROR; 
-	NODE* new = GB_GetSiteStdin(void);
-	InsertNodeAtPos(l, new, pos);
+	NODE* new = GB_GetSiteStdin(void);        /*the function that updates all the site's infos is called to the new node*/
+	InsertNodeAtPos(l, new, pos);	          /*then we insert the new node at the given position 'pos'*/
 	return SUCCESS;
 }
 
@@ -51,26 +53,26 @@ int GB_RemoveSite(LIST* l, int code){
 	if (GB_CheckEmptyList(l)) return ERROR;
 	NODE* aux1 = l->first; 
 	NODE* aux2 = aux1->next;	
-	if (aux1->code == code){ 		   //checks if the first node is to be erased, which will have 2 different cases
+	if (aux1->code == code){ 		  /*checks if the first node is to be erased, which will have 2 different cases*/
 		int size = GB_CountListSize(l);
-		if (size == 1) free(aux1);         //1: the list only has this node, so we only need to free it
-		else{				   //2: the list has more elements
+		if (size == 1) free(aux1);        /*1: the list only has this node, so we only need to free it*/
+		else{				  /*2: the list has more elements*/
 			l->first = aux2;
 			aux1->next = NULL;
 			free(aux1);
 			return SUCCESS;
 		}
 	}
-	while (aux2->code != pos && aux2 != NULL){ //runs through the list until it ends or until the code is found
+	while (aux2->code != pos && aux2 != NULL){ /*runs through the list until it ends or until the code is found*/
 		aux1 = aux1->next;
 		aux2 = aux2->next;
 	}
-	if (aux2 == NULL) return ERROR;            //if the code was not found, error is returned
-	if (aux2->code == code){		   //if the code was found, we start removing the node, but only after
-		aux1->next = aux2->next;           //linking correctly the nodes after and before the one to be removed
+	if (aux2 == NULL) return ERROR;            /*if the code was not found, error is returned*/
+	if (aux2->code == code){		   /*if the code was found, we start removing the node, but only after*/
+		aux1->next = aux2->next;           /*linking correctly the nodes after and before the one to be removed*/
 		aux2->next = NULL;
 		free(aux2);
 		return SUCCESS;
 	}
-	return ERROR;				   //if the function hasn't returned success yet, it has failed somewhere, so 
-}					           //we return error
+	return ERROR;				   /*if the function hasn't returned success yet, it has failed somewhere, so */
+}					           /*we return error*/
