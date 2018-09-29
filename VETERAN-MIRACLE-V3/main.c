958de08479e434bfba1774893334a8f1b41df144
João Villaça <38091668+ZeroDoisBCC018@users.noreply.github.com>
2018-09-28 23:03:07 -0300

    Add files via upload

--- VETERAN-MIRACLE-V3/main.c
+++ VETERAN-MIRACLE-V3/main.c
@@ -2,10 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
 #include "googlebot_lists.h"
 #include "googlebot_ds.c"
-
 #define CSV FILE*
 
 LIST* l;
@@ -17,11 +15,11 @@ void std_divide(void){
 }
 
 void print_first(void){
+	std_divide();
 	fprintf(stdout, "Modify the database: you can change the properties of a given site directly.\n");
 	fprintf(stdout, "Choose one of the following options:\n");
 	fprintf(stdout, "\t1 - Insert a new site;\n\t2 - Remove a site from the database;\n\t3 - Update the relevance of a specific site;\n");
 	fprintf(stdout, "\t4 - Insert a new keyword to a site;\n\t5 - Print this menu section again;\n\t6 - Return to the main menu.\n");
-	std_divide();
 }
 
 void new_site(void){
@@ -39,7 +37,7 @@ void new_site(void){
 	}
 	std_divide();
 	fprintf(stdout, "To insert your new site, please type these properties on the following order:\n");
-	fprintf(stdout, "(Press enter after each typed propertie.)\n");
+	fprintf(stdout, "(Press enter after each typed propertie)\n");
 	fprintf(stdout, "\t1 - Code (1 up to 9999);\n\t2 - Name (up to 50 characters);\n\t3 - Relevance (0 up to 1000);\n\t4 - Link (up to 100 characters);\n\t5 - Site tags/keywords (up to 10 tags, each with a max of 50 characters).\n");
 	GB_GetSiteStdin(l);
 }
@@ -58,8 +56,8 @@ void remove_site(void){
 		if (b == 0) return;
 	}
 	std_divide();
-	fprintf(stdout, "To remove a site, its code below:\n");
-	int code = fscanf(stdin,"%d", &code);
+	fprintf(stdout, "To remove a site, insert its code below:\n");
+	int code = fscanf(stdin, "%d", &code);
 	fprintf(stdin, "Are you sure? Insert code 1 to continue.\n");
 	int d = fscanf(stdin, "%d", &d);
 	if (d != 1){
@@ -92,7 +90,12 @@ void update_rel(void){
 	}
 	std_divide();
 	
-\z	if (GB_UpdateRelevance(l, c, r) != 1){
+	fprintf(stdout, "Insert the site's code:\n");
+	int code = fscanf(stdin, "%d", &code);
+	fprintf(stdout, "Insert the new relevance:\n");
+	int rel = fscanf(stdin, "%d", &rel);
+	
+	if (GB_UpdateRelevance(l, code, rel) != 1){
 		fprintf(stdout, "Relevance could not be updated. Return to the Modify Database menu section.\n");
 		return;
 	} else {
@@ -101,8 +104,9 @@ void update_rel(void){
 }
 
 void new_keyw(void){
+	
 	int d;
-	fprintf(stdout, "You're about to insert a new keyword into a specific site on the database.\n");
+	fprintf(stdout, "You're about to insert new tags into a specific site on the database.\n");
 	fprintf(stdout, "To cancel this operation, insert code 0. To continue please insert code 1.\n");
 	fscanf(stdin, "%d", &d);
 	if (d == 0) return;
@@ -113,12 +117,15 @@ void new_keyw(void){
 		if (d == 0) return;
 	}
 	std_divide();
+	fprintf(stdout,"Please insert the side code:\n");
+	int code = fscanf(stdin, "%d", &code);
+	fprintf(stdout, "Now insert the new tags: (max of 10 keywords, each up to 50 characters)\n");
 	
-	if (GB_InsertKeyword(l, d) != 1){
-		fprintf(stdout, "New keyword could not be inserted. Return to the Modify Database menu section.\n");
+	if (GB_InsertKeyword(l, code) != 1){
+		fprintf(stdout, "New keywords could not be inserted. Returning to the Modify Database menu section.\n");
 		return;
 	} else {
-		fprintf(stdout, "New keyword inserted succesfully.\n");
+		fprintf(stdout, "New keywords inserted succesfully.\n");
 	}
 }
 
@@ -137,7 +144,10 @@ void first(void){
 			case 3: update_rel(); break;
 			case 4: new_keyw(); break;
 			case 5: print_first(); break;
-			case 6: std_divide(); break;
+			case 6:
+				fprintf(stdout, "Returning to the main menu.\n");
+				std_divide(); 
+				break;
 			default:
 				fprintf(stdout, "Unknown Code! Please insert one of the option codes above.\n");
 		}
@@ -145,36 +155,32 @@ void first(void){
 	}
 }
 
-void second(void){
-	int d;
-	fprintf(stdout, "You are about to update the database.");
-	fprintf(stdout, "To cancel this operation, insert code 0. To continue please insert code 1.\n");
-	fscanf(stdin, "%d", &d);
-	if (d == 0) return;
-	while (d != 1){
-		fprintf(stdout, "Unknown code. To cancel this operation, insert code 0. To continue please insert code 1.\n");
-		fflush(stdin);
-		fscanf(stdin, "%d", &d);
-		if (d == 0) return;
-	}
+void second(CSV fp, char* fn){
+	
+	int op2 = 101;
+	
 	std_divide();
-	
-	if (GB_UpdateDataBase(fp, filename, l) != 1){
-		fprintf(stdout, "Database could not be updated. Return to the Modify Database menu section.\n");
-		return;
+	fprintf(stdout, "Update the database: this option will update your .csv file and print\n");
+	fprintf(stdout, "the current recorded site list. Be sure to make a backup before continuing.\n");
+	fprintf(stdout, "Insert code 1 to update the database, 0 to return to the main menu: ");
+	op2 = fscanf(stdin, "%d", &op2);
+	if (op2 == 1){
+		if (GB_UpdateDataBase(fp, fn, l)) fprintf(stdout, "Database updated successfully. Returning to the main menu.\n");
+		else {
+			perror("An error ocurred during the database updating proccess.\n");
+			exit(EXIT_FAILURE);
+		}
 	} else {
-		fprintf(stdout, "Database updated succesfully.\n");
-	
+		fprintf(stdout, "Returning to the main menu.\n");
 	}
 }
 
-void menu_options(void){
-	
+void menu_options(void){	
 	fprintf(stdout, "\t1 - Modify the database;\n\t2 - Update the database;\n\t3 - Print the menu options on the screen again;\n\t4 - Exit the Googlebot.\n");
 	std_divide();
 }
 
-void welcome(void){
+void welcome(CSV fp, char* fn){
 	fflush(stdin);
 	fprintf(stdout, "Googlebot SCC0202-2018 Project. Developed by Joao Villaca, Bruno Gazoni and Bruno Baldissera.\n");
 	fprintf(stdout, "ICMC/USP - Brazil, SP, Sao Carlos. This software is distributed as a free software under the\n");
@@ -182,10 +188,10 @@ void welcome(void){
 	fprintf(stdout, "for more information.\n");
 	std_divide();
 	fprintf(stdout, "Welcome. This is Googlebot alpha version 1.0. Choose one of the following options:\n");
-	menu();	
+	menu(fp, fn);	
 }
 
-void menu(void){
+void menu(CSV fp, char* fn){
 	
 	int op0 = 101;
 	menu_options();
@@ -196,9 +202,9 @@ void menu(void){
 		fscanf(stdin, "%d", &op0);
 		switch(op0){
 			case 1: first(); break;
-			case 2: second(); break;
+			case 2: second(fp, fn); break;
 			case 3: menu_options(); break;
-			case 4: break; 
+			case 4:	break; 
 			default:
 				fprintf(stdout, "Unknown Code! Please insert one of the option codes above.\n");
 		}
@@ -215,9 +221,7 @@ void error_no_name(void){
 	fprintf(stdout, "an empty new one with this name will be created.\n");
 }
 
-
-
-int main(int argc, char** argv){
+int main(int argc, char* argv[2]){
 	
 	if (argv[1] == NULL){ 
 		error_no_name();
@@ -225,7 +229,10 @@ int main(int argc, char** argv){
 		exit(EXIT_FAILURE);
 	} 
 	
-	CSV fp = GB_OpenCSVread(fp, argv[1]);
+	char* fn;
+	strcpy(fn, argv[1]);
+	
+	CSV fp = GB_OpenCSVread(fp, fn);
 	l = GB_NewList();
 	GB_ReadCSV(fp, l);
 	
@@ -235,7 +242,7 @@ int main(int argc, char** argv){
 		exit(EXIT_FAILURE);
 	}
 	
-	welcome();
+	welcome(fp, fn);
 	
 	return EXIT_SUCCESS;
 }
