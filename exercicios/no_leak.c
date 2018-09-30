b2982ac259725d866549a2c9da7a7fd08e39bbdc
João Villaça <38091668+ZeroDoisBCC018@users.noreply.github.com>
2018-09-30 15:57:45 -0300

    Update no_leak.c

--- exercicios/no_leak.c
+++ exercicios/no_leak.c
@@ -1,10 +1,14 @@
 #include <stdlib.h>
+#include <stddef.h>
 #include "no_leak.h"
 
 void** ptrs;
 
 void start_pointer_storage(void){
 	ptrs = (void**) malloc(500*sizeof(void*));
+	for(int i = 0; i < 500; i++){
+		ptrs[i] =NULL;
+	}
 }
 
 void add_pointer_to_storage(void* p){
@@ -17,4 +21,5 @@ void free_storage(void){
 	for(int i = 500; i > 0; i--){
 		free(ptrs[i]);
 	}
+	free(ptrs);
 }
