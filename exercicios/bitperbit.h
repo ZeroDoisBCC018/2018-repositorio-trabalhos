d576d6a3d656f68f3f1d485a1a95798a34759350
João Villaça <38091668+ZeroDoisBCC018@users.noreply.github.com>
2018-10-03 04:48:59 -0300

    Update bitperbit.h

--- exercicios/bitperbit.h
+++ exercicios/bitperbit.h
@@ -9,8 +9,10 @@ void swap_ints(int a, int b);
 
 unsigned int int_module(int n);
 
+//force \0 terminator in the last character of a string
 void force_terminator(char* str);
 
+//dump one byte (read as a character) only
 void fdump1(FILE* stream);
 
 void fdumpline(FILE* stream);
@@ -21,16 +23,23 @@ int higher(int a, int b);
 
 int lower(int a, int b);
 
+//least common multiple
 int lcm(int a, int b);
 
+//greatest common divisor
 int gcd(int a, int b);
 
 unsigned int factorial(int n);
 
 unsigned int fibonacci(int n);
 
+//mode = the file access mode after cleaning
 int cleanfile(FILE* fp, const char* mode);
 
+/* do an arithmetic operation by receiving the integer reference
+ * can be used in functions when you cant return the value
+ * op = the arithmetic operation simbol. note: this function reads
+ * its parameters in polish notation. */
 int int_arit_ref(const char op, int* n, int a);
 
 #endif
