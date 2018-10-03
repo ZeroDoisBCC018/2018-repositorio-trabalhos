99427d3047bf3087ff91b273353ec12ab788edb2
João Villaça <38091668+ZeroDoisBCC018@users.noreply.github.com>
2018-10-02 23:34:31 -0300

    Add files via upload

--- trabalhos_icc_2/spellcheck.c
+++ trabalhos_icc_2/spellcheck.c
@@ -1,3 +1,8 @@
+/*Spellcheck:
+Este programa serve como um corretor automatico baseado em livros
+utilizados como 'dicionario' para corrigir tweets obtidos por arquivos .json
+Trabalho 3 de ICC-II - Prof. Moacir Ponti - Entrega dia 02/10/2018
+Joao Vitor Nascimento Villaca - ICMC/USP - NoUSP: 10724239*/
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,6 +10,9 @@
 #include <ctype.h>
 #include <stdbool.h>
 
+/*
+	Busca binaria recursiva para o vetor de strings
+*/
 bool bsearch_strings(char** str1, char* str2, int st, int end){
 	
 	if(st >= end) return false;
@@ -20,6 +28,11 @@ bool bsearch_strings(char** str1, char* str2, int st, int end){
 	return true;
 }
 
+
+/*
+	Mergesort adaptado ao vetor de strings do programa
+	size e passado por referencia para ser modificado fora do escopo
+*/
 void mergesort_strings(char** v, int* size, int st, int end){
 	
 	int center = (int) ((end+st)/2.0);
@@ -31,7 +44,7 @@ void mergesort_strings(char** v, int* size, int st, int end){
 	char** aux = (char**) malloc(sizeof(char*) * h);
 	int g = 0;
 	for(g = 0; g < h; g++){
-		aux[g] = (char*) malloc(46*sizeof(char));
+		aux[g] = (char*) malloc(46 * sizeof(char));
 	}
 	
 	int i = st;
@@ -39,6 +52,7 @@ void mergesort_strings(char** v, int* size, int st, int end){
 	int k = 0;
 	int a, dif;
 	
+	//a interpolacao e baseada no retorno da funcao strcasecmp()
 	while(i <= center && j <= end){
 		
 		a = strcasecmp(v[i], v[j]);
@@ -63,12 +77,14 @@ void mergesort_strings(char** v, int* size, int st, int end){
 		k++;
 	}
 	
+	//size e modificado para servir de parametro da busca binaria posteriormente
 	*size -= dif;
 	
 	for(i = st, k = 0; i <= (end-dif); i++, k++){
 		strcpy(v[i], aux[k]);
 	}
 	
+	//essa string e jogada nas ultimas posicoes do vetor
 	char* last = "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
 	for(i = (end - dif); i < end; i++){
 		strcpy(v[i], last);
@@ -80,7 +96,7 @@ void mergesort_strings(char** v, int* size, int st, int end){
 	free(aux);
 }
 
-int main(){
+int main(int argc, const char * argv[]){
 	
 	char dc[64];
 	char tw[64];
@@ -91,24 +107,28 @@ int main(){
 	FILE* dcp = fopen(dc, "r");
 	FILE* twp = fopen(tw, "r");
 	
-	char** d = (char**) malloc(1000000*sizeof(char*));
+	char** d = (char**) malloc(1000000 * sizeof(char*));
 	int i = 0;
 	for(i = 0; i < 1000000; i++){
-		d[i] = (char*) malloc(46*sizeof(char));
+		d[i] = (char*) malloc(46 * sizeof(char));
 	}
 	
-	char** t = (char**) malloc(1000000*sizeof(char*));
+	char** t = (char**) malloc(1000000 * sizeof(char*));
 	for(i = 0; i < 1000000; i++){
-		t[i] = (char*) malloc(46*sizeof(char*));
+		t[i] = (char*) malloc(46 * sizeof(char*));
 	}
 	
+	/* sizes faz a contagem do numero
+	de strings que sao ocupadas em d */
 	i = 0;
-	do{
+	size_t sizes = 0;
+	while(feof(dcp) == 0) {
 		fscanf(dcp, "%[a-zA-Z]%*[^a-zA-Z]", d[i]);
+		sizes += strlen(d[i]);
 		i++;
-	} while(!feof(dcp));
+	}
 	
-	mergesort_strings(d, &i, 0, 1000000);
+	mergesort_strings(d, &i, 0, sizes);
 	
 	char *dump, *ss;
 	
@@ -118,25 +138,32 @@ int main(){
 	char c = 'q', x = 'q';
 	
 	do{
-		fscanf(twp, "%ms", &dump);
+		fscanf(twp, "%ms", &dump); //le as linhas do tweet
 	
 		ss = strstr(dump, "\"text\":");
-		if(ss != NULL){
+		if(ss != NULL){ //se tiver ao menos um tweet no json
 			
 			check = true;
 			
 			while(x != '\r' && x != '\n'){
 				
+				//enquanto o char for alfanumerico
 				while(isalpha(c)){
 					c = fgetc(twp);
+
+					//ele e adicionado na string
 					t[z][w] = c;
 					w++;
 				}
 				
+				//forcando o terminador \0
 				t[z][w] = '\0';
+
+				//variaveis reinicializadas para a proxima iteracao
 				w = 0;
 				c = 'q';
 				
+				//busca binaria da palavra em comum nos dois arquivos
 				if(bsearch_strings(d, t[z], 0, i)){
 					printf("%s ", t[z]);
 				}
@@ -147,7 +174,7 @@ int main(){
 		
 		printf("\n");
 		
-	} while(!feof(twp));
+	} while(feof(twp)==0);
 	
 	if(check == false) printf("No tweets to check\n");
 	
