ad39f573334c96c75076c264f70aa65b8cfb7cd1
João Villaça <38091668+ZeroDoisBCC018@users.noreply.github.com>
2018-10-03 04:42:23 -0300

    Update rec_hanoi.c

--- exercicios/rec_hanoi.c
+++ exercicios/rec_hanoi.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void recHanoi(int n, char orig, char meio, char dest){
-	if(n == 1) fprintf(stdout, "Mova de %c para %c\n", orig, dest);
+void recHanoi(int n, char orig, char mid, char dest){
+	if(n == 1) printf("Move from %c to %c\n", orig, dest);
 	else{
-		recHanoi((n-1), orig, dest, meio);
-		recHanoi(1, orig, meio, dest);
-		recHanoi((n-1), meio, orig, dest);
+		recHanoi((n-1), orig, dest, mid);
+		recHanoi(1, orig, mid, dest);
+		recHanoi((n-1), mid, orig, dest);
 	}
 }
 
-int main(){
-	fprintf(stdout, "Algoritmo recursivo de Hanoi para 3 torres\n");
-	fprintf(stdout, "Digite o numero de discos (nao recomendado para mais de 10 discos):");
+int main(int argc, char* argv[]){
 	 
 	char a = 'A', b = 'B', c = 'C';
-	int n = fscanf(stdin, "%d", &n);
-	 
+	int n = atoi(argv[1]);
+	
 	recHanoi(n, a, b, c);
-	exit(EXIT_SUCCESS);
+	
+	return 0;
 }
