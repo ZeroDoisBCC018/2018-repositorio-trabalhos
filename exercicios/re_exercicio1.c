ba62e3d3b1fd2cd471e155b0b291778e06ff9906
João Villaça <38091668+ZeroDoisBCC018@users.noreply.github.com>
2018-11-16 22:52:23 -0200

    Create re_exercicio1.c

---
+++ exercicios/re_exercicio1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/*  Este codigo esta com poucos tratamentos de excecao,
+    poucos erros ja levariam a comportamento indefinido
+    (instavel). O objetivo eh achar pelo menos 3 formas
+    de cagar o objeto por completo.
+*/
+
+typedef struct _stdobject_{
+    va_list args;
+    void* morph;
+    va_list _arguments(int n_args){
+        va_list this;
+        va_start(this, n_args);
+        return this;
+    }
+    void _end_arg(va_list a){
+        va_end(a);
+        return;
+    }
+    void* _get_type(const char[] type){
+        void* deftype = NULL;
+        char let = type[0];
+        int invalid = 0;
+        do{
+            switch(let){
+                case i:
+                    return (int*)deftype;
+                case c:
+                    if(type[1] == 'h') return (char*)deftype;
+                    else let = type[7];
+                    invalid++;
+                    continue;
+                case f:
+                case d:
+                case l:
+                case s:
+                case u:
+                    let = type[10];
+                    invalid++;
+                    continue;
+                case a: 
+                    let = type[6];
+                    invalid++;
+                    continue;
+            }
+        }while(invalid < 2);
+        return deftype;
+    }
+}object;
